9868662396c63ee0fd460dca6e08948f835e0825
Mike North <michael.l.north@gmail.com>
2019-04-23 13:11:37 -0700

    assert element capture is successful in MessageCreator#sendMessage

--- app/components/message-creator.js
+++ app/components/message-creator.js
@@ -6,12 +6,18 @@ export default class MessageCreatorComponent extends Component {
   @tracked
   messageBuffer = '';
 
+  element = null;
+
   get submitDisabled() {
     return !this.messageBuffer;
   }
 
   @action
   async sendChatMessage(evt) {
+    if (!this.element)
+      throw new Error(
+        'MessageCreator sendChatMessage while element has not yet been captured'
+      );
     evt.preventDefault();
 
     await this.args.onSubmit(this.messageBuffer);
