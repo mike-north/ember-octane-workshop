451094e2e8cc176d931e1522de67aa66bdc8b4d8
John Robinson <jorobinson@linkedin.com>
2019-04-22 17:41:39 -0700

    a11y/semantic markup changes, etc.

--- app/components/message-creator.js
+++ app/components/message-creator.js
@@ -5,11 +5,19 @@ import { tracked } from '@glimmer/tracking';
 export default class MessageCreatorComponent extends Component {
   @tracked
   messageBuffer = '';
+
+  get submitDisabled() {
+    return !this.messageBuffer;
+  }
+
   @action
   async sendChatMessage(evt) {
     evt.preventDefault();
+
     await this.args.onSubmit(this.messageBuffer);
+
     this.messageBuffer = '';
+    this.messageInput.focus();
   }
 
   @action
@@ -17,7 +25,9 @@ export default class MessageCreatorComponent extends Component {
     this.messageBuffer = evt.target.value;
   }
 
-  get submitDisabled() {
-    return !this.messageBuffer;
+  @action
+  captureMessageInput(element) {
+    this.messageInput = element;
+    this.messageInput.focus();
   }
 }
