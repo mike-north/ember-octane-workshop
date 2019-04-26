d114bef32ba2c20543b9456bf194fdce981cca25
Mike North <michael.l.north@gmail.com>
2019-04-26 13:06:11 -0700

    11 - async data

--- app/routes/teams/team/channel.js
+++ app/routes/teams/team/channel.js
@@ -1,9 +1,11 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
 export default class TeamsTeamChannelRoute extends Route {
-  model({ channelId }) {
-    const team = this.modelFor('teams.team');
-    const matches = team.channels.filter(ch => `${ch.id}` === `${channelId}`);
-    return matches[0];
+  async model({ channelId }) {
+    const { teamId } = this.paramsFor('teams.team');
+    // const team = this.modelFor('teams.team');
+    const resp = await fetch(`/api/teams/${teamId}/channels/${channelId}`);
+    return resp.json();
   }
 }
