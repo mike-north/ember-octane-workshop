9d2ef52715f93cb415895682428069fb74deaffd
Mike North <michael.l.north@gmail.com>
2019-04-26 09:33:58 -0700

    add smoke bg color

--- app/tailwind/config/colors.js
+++ app/tailwind/config/colors.js
@@ -98,4 +98,12 @@ export default {
   'pink-light': '#fa7ea8',
   'pink-lighter': '#ffbbca',
   'pink-lightest': '#ffebef',
+
+  'smoke-darkest': 'rgba(0, 0, 0, 0.9)',
+  'smoke-darker': 'rgba(0, 0, 0, 0.75)',
+  'smoke-dark': 'rgba(0, 0, 0, 0.6)',
+  'smoke': 'rgba(0, 0, 0, 0.5)',
+  'smoke-light': 'rgba(0, 0, 0, 0.4)',
+  'smoke-lighter': 'rgba(0, 0, 0, 0.25)',
+  'smoke-lightest': 'rgba(0, 0, 0, 0.1)',
 };
