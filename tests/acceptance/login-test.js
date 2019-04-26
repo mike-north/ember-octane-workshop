92da6f1fdb900aec6adb2979cfa97f0b1be6a4fb
Mike North <michael.l.north@gmail.com>
2019-04-26 13:27:05 -0700

    15 - fowarding routes, simulating API responses in tests

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -25,7 +25,7 @@ module('Acceptance | login', function(hooks) {
 
     await click('form input[type="submit"]');
 
-    assert.equal(currentURL(), '/teams');
+    assert.ok(currentURL().startsWith('/teams'));
   });
 
   test('already logged in', async function(assert) {
@@ -37,6 +37,6 @@ module('Acceptance | login', function(hooks) {
 
     await visit('/login');
 
-    assert.equal(currentURL(), '/teams');
+    assert.ok(currentURL().startsWith('/teams'));
   });
 });
