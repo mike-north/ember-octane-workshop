610c5bfac850ecf0d87a0aa588fbaecaf15bd403
Mike North <michael.l.north@gmail.com>
2019-04-26 13:00:05 -0700

    13 - route models

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -2,6 +2,52 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import AuthService from 'shlack/services/auth';
 
+export const ALL_TEAMS = [
+  {
+    id: 'li',
+    name: 'LinkedIn',
+    order: 2,
+    iconUrl:
+      'https://gravatar.com/avatar/0ca1be2eaded508606982feb9fea8a2b?s=200&d=https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/240px-LinkedIn_logo_initials.png',
+    channels: [
+      {
+        id: 'general',
+        name: 'general',
+        description: 'LinkedIn general (professional) chat',
+        teamId: 'li',
+      },
+      {
+        id: 'secrets',
+        name: 'secrets',
+        description: 'professional secrets',
+        teamId: 'li',
+      },
+    ],
+  },
+  {
+    id: 'ms',
+    name: 'Microsoft',
+    order: 3,
+    iconUrl:
+      'https://gravatar.com/avatar/0ca1be2eaded508606982feb9fea8a2b?s=200&d=https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/200px-Microsoft_logo.svg.png',
+    channels: [
+      {
+        id: 'general',
+        name: 'general',
+        description: 'Microsoft general chat',
+        teamId: 'ms',
+      },
+      {
+        id: 'ie8-gripes',
+        name: 'IE8 Gripes',
+        description:
+          'A place for whining about old browsers',
+        teamId: 'ms',
+      },
+    ],
+  },
+];
+
 export default class TeamsRoute extends Route {
   /**
    * @type {AuthService}
@@ -13,5 +59,9 @@ export default class TeamsRoute extends Route {
     if (!this.auth.isAuthenticated) {
       this.transitionTo('login');
     }
+  }
+
+  model() {
+    return ALL_TEAMS;
   }
 }
