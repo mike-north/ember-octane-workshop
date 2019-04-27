29e72cfbbc56c8893c33930aaea79479bde9a598
Mike North <michael.l.north@gmail.com>
2019-04-27 11:15:24 -0700

    18 - PWA

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,11 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {});
+  let app = new EmberApp(defaults, {
+    'esw-cache-fallback': {
+      patterns: ['/api/(.+)']
+    }
+  });
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
