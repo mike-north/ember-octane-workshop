5a7cd8b74ac6bf429b3e15e2419d91a71a6829ec
Mike North <michael.l.north@gmail.com>
2019-04-29 21:09:49 -0500

    10 - auth service and login flow

---
+++ tests/integration/components/team-sidebar-test.js
@@ -0,0 +1,23 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | team-sidebar', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<TeamSidebar />`);
+
+    assert.deepEqual(
+      this.element.textContent
+        .trim()
+        .replace(/\s*\n+\s*/g, '\n')
+        .split('\n'),
+      ['LinkedIn', 'Mike North (1)', 'Channels', '#', 'general', 'Logout']
+    );
+  });
+});
