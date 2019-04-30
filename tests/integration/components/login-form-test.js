3eb1ec93f29d6d2877871f0bc62551bdd6808ea1
Mike North <michael.l.north@gmail.com>
2019-04-29 20:40:35 -0500

    09 - component integration tests

--- tests/integration/components/login-form-test.js
+++ tests/integration/components/login-form-test.js
@@ -1,15 +1,12 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find, fillIn } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | login-form', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('initially has no user selected, and "Sign In" button disabled', async function(assert) {
     await render(hbs`<LoginForm />`);
 
     assert.deepEqual(
@@ -18,6 +15,48 @@ module('Integration | Component | login-form', function(hooks) {
         .replace(/\s*\n+\s*/g, '\n')
         .split('\n'),
       ['Login', 'Select a user', 'Testy Testerson', 'Sample McData']
+    );
+
+    let button = /** @type {HTMLInputElement} */ (find('input[type="submit"]'));
+    let select = /** @type {HTMLSelectElement} */ (find('select'));
+
+    assert.equal(select.value, '');
+
+    assert.equal(button.disabled, true);
+  });
+
+  test('after selecting a user "Sign In" button enabled', async function(assert) {
+    // Render the component
+    await render(hbs`<LoginForm />`);
+
+    // Pluck off the DOM elements we care about
+    let button = /** @type {HTMLInputElement} */ (find('input[type="submit"]'));
+    let select = /** @type {HTMLSelectElement} */ (find('select'));
+
+    // Select the <option> with value="1"
+    await fillIn('select', '1');
+
+    assert.equal(select.value, '1', '<option value="1"> is currently selected');
+
+    assert.equal(
+      button.disabled,
+      false,
+      'The submit button is no longer disabled'
+    );
+
+    assert.deepEqual(
+      this.element.textContent
+        .trim()
+        .replace(/\s*\n+\s*/g, '\n')
+        .split('\n'),
+      [
+        'Login',
+        'Select a user',
+        'Testy Testerson',
+        'Sample McData',
+        'Logging in with userId 1',
+      ],
+      'validation text now shows up'
     );
   });
 });
