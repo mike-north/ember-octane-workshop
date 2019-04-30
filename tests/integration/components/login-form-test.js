231327b45f928b354b438c6d5ecde268e3a9654f
Mike North <michael.l.north@gmail.com>
2019-04-29 20:34:16 -0500

    07 - login form, and handling the submit event w/ an action

---
+++ tests/integration/components/login-form-test.js
@@ -0,0 +1,29 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | login-form', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<LoginForm />`);
+
+    assert.deepEqual(
+      this.element.textContent
+        .trim()
+        .replace(/\s*\n+\s*/g, '\n')
+        .split('\n'),
+      [
+        'Login',
+        'Select a user',
+        'Testy Testerson',
+        'Sample McData',
+        'A validation message',
+      ]
+    );
+  });
+});
