30e6218804f29eca76557eece248b85d48e973ce
Mike North <michael.l.north@gmail.com>
2019-05-02 23:42:42 -0700

    fix node linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,18 @@ module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['ember'],
   extends: ['eslint:recommended', 'plugin:ember/recommended'],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
     'ember/routes-segments-snake-case': 0,
     'no-debugger': 0,
     'no-console': 0,
-    'no-unused-vars': 0
+    'no-unused-vars': 0,
   },
   overrides: [
     // node files
@@ -22,30 +22,33 @@ module.exports = {
       files: [
         '.ember-cli.js',
         '.eslintrc.js',
+        'prod-server.js',
+        'server/**/*.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
       ],
       excludedFiles: ['app/**'],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015
+        ecmaVersion: 2015,
       },
       env: {
         browser: false,
-        node: true
+        node: true,
       },
       plugins: ['node'],
       rules: Object.assign(
         {},
+        // eslint-disable-next-line node/no-unpublished-require
         require('eslint-plugin-node').configs.recommended.rules,
         {
           // add your custom rules and overrides for node files here
         }
-      )
-    }
-  ]
+      ),
+    },
+  ],
 };
--- server/api-server.js
+++ server/api-server.js
@@ -4,21 +4,17 @@ const url = require('url');
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 // eslint-disable-next-line no-unused-vars
-const express = require('express');
 
 module.exports = server => {
   function SINGULAR_MIDDLEWARE(req, res, next) {
     const _send = res.send;
     res.send = function(body) {
-      if (url.parse(req.url, true).query['singular']) {
+      if (new url.URL(req.url, true).query['singular']) {
         try {
           const json = JSON.parse(body);
           if (Array.isArray(json)) {
             if (json.length === 1) {
-              return _send.call(
-                this,
-                JSON.stringify(json[0])
-              );
+              return _send.call(this, JSON.stringify(json[0]));
             } else if (json.length === 0) {
               return _send.call(this, '{}', 404);
             }
