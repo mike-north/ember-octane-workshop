e993b4c8ba754d7b26b70e81364a61a43590fa21
Melanie Sumner <melaniersumner@gmail.com>
2019-08-02 16:10:21 -0500

    Update date.js to handle 12PM correctly (#34)
    * Update date.js
    
    If merged, this PR updates the date.js file to handle 12 noon times. Otherwise it returns 00 as the hour with both AM and PM.
    
    * Update date.js
    
    consolidating code based on review
    
    * Update date.js

--- starter-files/date.js
+++ starter-files/date.js
@@ -47,14 +47,18 @@ export function dateToString(date) {
   )
     return null;
   const d = new Date(date);
-  const ampm = d.getHours() > 12 ? 'PM' : 'AM';
+  const ampm = d.getHours() >= 12 ? 'PM' : 'AM';
+  const hrsConverted = ( d.getHours() === 0 || d.getHours() > 12 ) ?
+                          Math.abs( 12 - d.getHours() ) :
+                          d.getHours();
+
   return `${
     MONTH_NAMES[d.getMonth()]
   } ${d.getDate()}, ${d.getFullYear()} ${padLeadingZeroes(
-    d.getHours() % 12,
+    hrsConverted,
     2
   )}:${padLeadingZeroes(
     d.getMinutes(),
     2
-  )}:${padLeadingZeroes(d.getSeconds(), 2)} ${ampm}`;
+  )}.${padLeadingZeroes(d.getSeconds(), 2)} ${ampm}`;
 }
