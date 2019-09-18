28ad5568af92f153373db44bb2c6d910f5a7f85c
Mike North <michael.l.north@gmail.com>
2019-09-18 10:35:28 -0700

    2019-09-18

---
+++ tests/acceptance/logout-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { visit, click, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | logout', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('clicking the logout button should result in us arriving at the login screen', async function(assert) {
+    await visit('/teams');
+
+    await click('.team-sidebar__logout-button');
+
+    assert.equal(currentURL(), '/login');
+  });
+});
