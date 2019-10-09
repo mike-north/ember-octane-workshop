e598ef69172d3c19a57e6c9aff1971a43e274df5
Mike North <michael.l.north@gmail.com>
2019-10-09 16:10:57 +0530

    Step 12: Guarded routes

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class LoginRoute extends Route {
+  @service auth;
+  @service router;
+  async beforeModel() {
+    await super.beforeModel(); // parent work
+    if (this.auth.currentUserId) {
+      this.router.transitionTo('teams');
+    }
+  }
 }
--- app/routes/teams.js
+++ app/routes/teams.js
@@ -3,8 +3,14 @@ import { inject as service } from '@ember/service';
 
 export default class TeamsRoute extends Route {
   @service auth;
+  @service router;
   async beforeModel() {
     await super.beforeModel(); // parent work
+    if (!this.auth.currentUserId) {
+      // send the user to the login screen
+      this.router.transitionTo('login');
+      return;
+    }
     await this.auth.fetchUserRecord(); // my work
   }
   // "fetch data from my API"
